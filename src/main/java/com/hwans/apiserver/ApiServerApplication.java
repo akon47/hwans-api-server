5a93b38b8edad96d2f7152db25f753955fb56a99
Kim, Hwan <akon47@naver.com>
2022-07-12 21:42:20 +0900

    first commit

---
+++ src/main/java/com/hwans/apiserver/ApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.hwans.apiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hwans/apiserver/ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwans.apiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
