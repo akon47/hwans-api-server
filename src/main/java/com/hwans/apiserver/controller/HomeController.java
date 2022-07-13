16ddd459b43cf7c773d2c059847a1aa11bf38e0d
Kim, Hwan <akon47@naver.com>
2022-07-13 23:12:50 +0900

    자동 빌드 테스트를 위한 커밋

---
+++ src/main/java/com/hwans/apiserver/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.hwans.apiserver.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello, World!";
+    }
+
+}
