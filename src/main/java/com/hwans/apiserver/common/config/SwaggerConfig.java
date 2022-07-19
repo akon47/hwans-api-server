e36ef975ee08ea3b8e6ed9d59bf93b2194806dea
Kim, Hwan <akon47@naver.com>
2022-07-20 02:05:16 +0900

    Swagger에 인증 토큰 정보를 입력할 수 있는 기능을 추가합니다.

--- src/main/java/com/hwans/apiserver/common/config/SwaggerConfig.java
+++ src/main/java/com/hwans/apiserver/common/config/SwaggerConfig.java
@@ -5,9 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -15,12 +20,13 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
-                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(authenticationSchemes())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.hwans.apiserver.controller"))
                 .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
+                .build();
     }
 
     private ApiInfo apiInfo() {
@@ -31,4 +37,24 @@ public class SwaggerConfig {
                 .build();
     }
 
+    private List<SecurityScheme> authenticationSchemes() {
+        HttpAuthenticationScheme authenticationScheme = HttpAuthenticationScheme
+                .JWT_BEARER_BUILDER
+                .name("JWT")
+                .build();
+        return Collections.singletonList(authenticationScheme);
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
 }
