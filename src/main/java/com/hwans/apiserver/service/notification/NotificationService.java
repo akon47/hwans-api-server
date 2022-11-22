439f5cf2da95d9b766c28aab8d1cad27559fb089
Kim,Hwan <akon47@naver.com>
2022-11-23 01:57:05 +0900

    웹소켓 관련 코드를 제거합니다.

--- src/main/java/com/hwans/apiserver/service/notification/NotificationService.java
+++
@@ -1,7 +0,0 @@
-package com.hwans.apiserver.service.notification;
-
-import com.hwans.apiserver.dto.notification.NotificationMessageDto;
-
-public interface NotificationService {
-    void SendNotificationMessage(NotificationMessageDto notificationMessageDto);
-}
