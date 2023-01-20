59f557c194a317e41f6a3d8f005f8c2c043da215
Kim, Hwan <akon47@naver.com>
2023-01-20 12:17:00 +0900

    알림 관련 Dto를 추가합니다.

---
+++ src/main/java/com/hwans/apiserver/dto/notification/NotificationDto.java
@@ -0,0 +1,14 @@
+package com.hwans.apiserver.dto.notification;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+/**
+ * 알림 Dto
+ */
+@Getter
+@Builder
+public class NotificationDto implements Serializable {
+}
--- src/main/java/com/hwans/apiserver/entity/notification/NotificationType.java
+++ src/main/java/com/hwans/apiserver/entity/notification/NotificationType.java
@@ -1,6 +1,12 @@
 package com.hwans.apiserver.entity.notification;
 
-// 알림 유형
+
+/**
+ * 알림 유형
+ */
 public enum NotificationType {
+    /**
+     * 새로운 댓글 알림
+     */
     NEW_COMMENT,
 }
