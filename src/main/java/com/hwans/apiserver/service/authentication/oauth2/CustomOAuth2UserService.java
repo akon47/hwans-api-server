b5c944366e3d781368edb7220db64262cd23ee43
Kim, Hwan <akon47@naver.com>
2023-03-10 00:21:53 +0900

    OAuth 로그인 시 생성된 Access 토큰을 서버가 발급한 토큰으로 기록하지 않던 문제를 수정합니다.

--- src/main/java/com/hwans/apiserver/service/authentication/oauth2/CustomOAuth2UserService.java
+++ src/main/java/com/hwans/apiserver/service/authentication/oauth2/CustomOAuth2UserService.java
@@ -1,9 +1,11 @@
 package com.hwans.apiserver.service.authentication.oauth2;
 
+import com.hwans.apiserver.common.Constants;
 import com.hwans.apiserver.common.security.jwt.JwtTokenProvider;
 import com.hwans.apiserver.entity.account.role.Role;
 import com.hwans.apiserver.repository.account.AccountRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -12,6 +14,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,6 +23,7 @@ import java.util.stream.Collectors;
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     private final AccountRepository accountRepository;
     private final JwtTokenProvider jwtTokenProvider;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Transactional
     @Override
@@ -40,6 +44,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
                     .collect(Collectors.toSet());
             var token = jwtTokenProvider.createToken(foundAccount.getEmail(), authorities);
             accountRepository.save(foundAccount.withRefreshToken(token.getRefreshToken()));
+            redisTemplate.opsForValue().set(token.getAccessToken(), "issue", Duration.ofMillis(Constants.ACCESS_TOKEN_EXPIRES_TIME));
             return new CustomOAuth2User(providerAttributes, authorities, token.getAccessToken(), token.getRefreshToken());
         } else {
             var registerToken = jwtTokenProvider.createRegisterToken(providerAttributes.getEmail());
