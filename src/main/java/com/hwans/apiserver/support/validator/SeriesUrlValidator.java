978c3f46a69108b6d529431e333b54ec28105521
Kim, Hwan <akon47@naver.com>
2023-04-18 19:34:49 +0900

    시리즈 URL 유효성 검사 클래스를 수정합니다.

--- src/main/java/com/hwans/apiserver/support/validator/SeriesUrlValidator.java
+++ src/main/java/com/hwans/apiserver/support/validator/SeriesUrlValidator.java
@@ -1,6 +1,6 @@
 package com.hwans.apiserver.support.validator;
 
-import com.hwans.apiserver.support.annotation.PostUrl;
+import com.hwans.apiserver.support.annotation.SeriesUrl;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -8,7 +8,7 @@ import javax.validation.ConstraintValidatorContext;
 /**
  * SeriesUrl에 대한 유효성 검사 기능을 제공한다.
  */
-public class SeriesUrlValidator implements ConstraintValidator<PostUrl, String> {
+public class SeriesUrlValidator implements ConstraintValidator<SeriesUrl, String> {
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
