401fe4a1efd911f1d6d51046fe0e2f41ea0458e5
Kim, Hwan <akon47@naver.com>
2024-11-07 13:15:50 +0900

    인기 게시글 페이징이 이상하게 동작하는 문제를 수정합니다.

--- src/main/java/com/hwans/apiserver/repository/blog/PostRepository.java
+++ src/main/java/com/hwans/apiserver/repository/blog/PostRepository.java
@@ -23,7 +23,7 @@ public interface PostRepository extends JpaRepository<Post, UUID> {
     List<Post> findAllByOrderByIdDesc(Pageable page);
 
     @Query("select x from Post as x where x.deleted = false and x.openType = 'PUBLIC' and x.id < :id")
-    List<Post> findByIdLessThanOrderByIdDesc(@Param("uuid") UUID id, @Param("createdAt") LocalDateTime createdAt, Pageable page);
+    List<Post> findByIdLessThanOrderByIdDesc(@Param("uuid") UUID id, Pageable page);
 
     @Query("select distinct post from Post as post left outer join post.postTags as postTag left outer join postTag.tag as tag where post.deleted = false and (:findPublicPostOnly is false or post.openType = 'PUBLIC') and post.account.blogId = :blogId and (:tag is null or tag.name = :tag) order by post.createdAt desc, post.id desc")
     List<Post> findAllByOrderByIdDesc(@Param("blogId") String blogId, @Param("tag") String tag, @Param("findPublicPostOnly") boolean findPublicPostOnly, Pageable page);
@@ -35,7 +35,7 @@ public interface PostRepository extends JpaRepository<Post, UUID> {
     List<Post> findAllByOrderByIdDesc(@Param("search") String search, Pageable page);
 
     @Query("select x from Post as x where x.deleted = false and x.openType = 'PUBLIC' and x.id < :id and (x.title like concat('%',:search,'%') or x.content like concat('%',:search,'%'))")
-    List<Post> findByIdLessThanOrderByIdDesc(@Param("uuid") UUID id, @Param("createdAt") LocalDateTime createdAt, @Param("search") String search, Pageable page);
+    List<Post> findByIdLessThanOrderByIdDesc(@Param("uuid") UUID id, @Param("search") String search, Pageable page);
 
     @Query("select count(*) from Post as x where x.deleted = false and (:findPublicPostOnly is false or x.openType = 'PUBLIC') and x.account.blogId = :blogId")
     int getCountByBlogId(@Param("blogId") String blogId, @Param("findPublicPostOnly") boolean findPublicPostOnly);
